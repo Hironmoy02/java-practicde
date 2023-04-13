d419cdd9b60f5bbc3e3d85c13bcc6c39ea0f3b03
Hironmoy02 <93079200+Hironmoy02@users.noreply.github.com>
2023-04-13 12:47:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       int num1=10;
+       int num2=45;
+       int num3=14;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
